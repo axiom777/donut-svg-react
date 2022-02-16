85835d4969c7d692ebdbf24497353dd49063d30c
axiom777 <axiom777@yandex.ru>
2022-02-16 11:14:03 +0300

    Create Legend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Donut from "./components/Donut/Donut";
+import Legend from "./components/Legend/Legend";
 
 function App() {
   return (
     <div>
+      <Legend/>
+      <br />
       <Donut size="small" percent={100} direction="down" difference={-3} />
       <Donut size="regular" percent={75} direction="down" difference={15} />
       <Donut size="regular" percent={50} direction="down" difference={-3} />
---
+++ src/components/Legend/Legend.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import styles from "./Legend.module.scss";
+import { colors } from "../constants";
+
+export default class Legend extends React.Component {
+  render() {
+    const height = 32;
+    const count = colors.length;
+    return (
+      <div className={styles.wrapper}>
+        {colors.map(({ percent, color, name }, index) => {
+          const isLast = index === colors.length - 1;
+          const endPercent = !isLast ? colors[index + 1].percent : 100;
+          return (
+            <div
+              key={index}
+              className={styles.item}
+              style={{
+                maxWidth: `calc(100% / ${count})`,
+                minWidth: `calc(100% / ${count})`,
+              }}
+            >
+              <div
+                className={styles.bar}
+                style={{
+                  backgroundColor: color,
+                  height: `${height}px`,
+                  width: `calc(${isLast ? 0 : height}px + 100% / ${count})`,
+                  borderRadius: `${height / 2}px`,
+                  left: `calc((100% /${count})*${index})`,
+                }}
+              >
+                <span
+                  style={{
+                    width: `calc(100% - ${isLast ? 0 : height}px)`,
+                    display: "block",
+                    textAlign: "center",
+                  }}
+                >
+                  {percent}-{endPercent}%
+                </span>
+              </div>
+              <div
+                className={styles.name}
+                style={{
+                  marginTop: `${height + 5}px`,
+                  color,
+                }}
+              >
+                {name}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
