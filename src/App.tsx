7c4d1026b7fe1d47bca601b7aea86bc1e0821007
axiom777 <axiom777@yandex.ru>
2022-02-18 12:56:18 +0300

    Move on fiew pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "./App.css";
 import Charts from "./components/Charts/Charts";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Table from "./components/Table/Table";
+import styles from "./App.module.scss";
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Charts />
+      <div className={styles.wrapper}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/charts" element={<Charts />} />
+            <Route path="/table" element={<Table />} />
+            <Route path="*" element={<div>Not Found</div>} />
+            <Route
+              path="/"
+              element={
+                <ul className={styles.list}>
+                  <li>
+                    <Link to="/charts">Бублики</Link>
+                  </li>
+                  <li>
+                    <Link to="/table">Таблица</Link>
+                  </li>
+                </ul>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
