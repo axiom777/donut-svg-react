3dfccc69ea1296a453b00df554e4682799363dc3
axiom777 <axiom777@yandex.ru>
2022-02-21 08:35:33 +0300

    Config for slice module

--- src/components/Table/Slice/Slice.tsx
+++ src/components/Table/Slice/Slice.tsx
@@ -4,12 +4,44 @@ import styles from "./Slice.module.scss";
 type Props = {
   percent: number;
   slices: number[];
-  background?: string;
+  type: keyof typeof config;
+};
+
+const colors = {
+  red: "#AC2020",
+  yellow: "#D38235",
+  green: "#009CA6",
+};
+
+const config = {
+  plan: [
+    { value: 50, color: colors.red },
+    { value: 94, color: colors.yellow },
+    { value: 100, color: colors.green },
+  ],
+  comment: [
+    { value: 19, color: colors.green },
+    { value: 49, color: colors.yellow },
+    { value: 100, color: colors.red },
+  ],
 };
 
 export default class Slice extends React.Component<Props, {}> {
   render() {
-    const { percent, slices, background } = this.props;
+    const { percent, slices, type } = this.props;
+    const currentConfig = config[type];
+
+    let color = currentConfig[0].color;
+
+    for (let i = 1; i < currentConfig.length; i++) {
+      const prevValue = currentConfig[i - 1].value;
+      const currentValue = currentConfig[i].value;
+      if (prevValue < percent) {
+        color = currentConfig[i].color;
+      }
+
+      if (currentValue > percent) break;
+    }
 
     return (
       <div className={styles.wrapper}>
@@ -19,7 +51,7 @@ export default class Slice extends React.Component<Props, {}> {
             className={styles.percent}
             style={{
               right: `${100 - percent}%`,
-              backgroundColor: background ? background : "red",
+              backgroundColor: color,
             }}
           />
           {slices.map((slice, index) => (
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import styles from "./Table.module.scss";
 import Slice from "./Slice/Slice";
 
 export default class Table extends React.Component {
   render() {
     return (
-      <div>
-        <Slice percent={75} slices={[50, 95]} background='#D38235'  />
-        <Slice percent={25} slices={[20, 50]} background='#009CA6'  />
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <span className={styles.bold}>Оценка качества планирования:</span>
+          <span>План сформирован на </span>
+          <Slice percent={75} slices={[50, 95]} type="plan" />
+          <span>Рецензентом даны комментариев </span>
+          <Slice percent={18} slices={[20, 50]} type="comment" />
+        </div>
       </div>
     );
   }
